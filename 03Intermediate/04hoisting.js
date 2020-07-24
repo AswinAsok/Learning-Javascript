d1812e2fec27185989e1a299403c3b539ae1e22a
AswinAsok <aswinasokofficial@gmail.com>
2020-07-24 14:54:49 +0530

    Day 17

---
+++ 03Intermediate/04hoisting.js
@@ -0,0 +1,27 @@
+tipper("80");
+
+function tipper(a){
+    var bill = parseInt(a);
+    console.log(bill+5);
+}
+
+
+
+var bigTipper = function (a){
+    var bill = parseInt(a)
+    console.log(bill+15);
+}
+
+bigTipper("200");
+
+console.log(name);
+var name = "Hilarious";
+
+
+function sayName(){
+    var name = "Mr Coder";
+    console.log(`From Function ${name}`);
+}
+sayName();
+
+console.log(name);
