9ca8ab33efa497a32c30d46762afd2341fb21612
AswinAsok <aswinasokofficial@gmail.com>
2020-07-29 14:56:33 +0530

    Callback and Arrow Function

---
+++ 03Intermediate/08Callback.js
@@ -0,0 +1,19 @@
+var isEven = (element) => {
+//     if (element%2===0) {
+//         return true
+//     }
+//     return false
+// }
+    
+    return element % 2 === 0
+};
+
+
+
+console.log(isEven(2));
+
+var result = [2, 4, 6, 9].every(isEven)
+console.log(result);
+
+var result = [2, 4, 6, 8].every((e) => e%2 === 0);
+console.log(result);
