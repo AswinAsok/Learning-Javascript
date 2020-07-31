e5dc094f6c111747a1e1cfcd6eeb6a954931296f
AswinAsok <aswinasokofficial@gmail.com>
2020-07-31 15:24:39 +0530

    Fill and Filter

---
+++ 03Intermediate/09FillandFilter.js
@@ -0,0 +1,9 @@
+const testArray = [2, 4, 6, 8 , 2, 9, 0, 2]
+
+console.log(testArray.fill("empty"));
+
+const  myNumbers = [20, 43, 15, 46, 17, 58, 98, 07, 01];
+
+const result = myNumbers.filter((number) => number < 50 )
+
+console.log(result);
