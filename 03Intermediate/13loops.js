51c15f2a572b09062235e64f2943a743d9a981d3
AswinAsok <aswinasokofficial@gmail.com>
2020-08-06 13:52:51 +0530

    for each loop

---
+++ 03Intermediate/13loops.js
@@ -0,0 +1,21 @@
+
+const myStates = [
+    "Kerala",
+    "Tamilnadu",
+    "Karnataka",
+    1974,
+    "Uttar Pradesh",
+    "Delhi",
+    "Assam"
+];
+
+// var i=0;
+ 
+// for(;;){
+//     if(i>3) break;
+//     console.log(i);
+//     i++;
+// }
+
+myStates.forEach((s) => (console.log(s)));
+myStates.forEach((s) => (console.log("We want Projects")));
