aa4a7211a5685874039667a1c64679cede815008
AswinAsok <aswinasokofficial@gmail.com>
2020-08-08 17:59:28 +0530

    More Loops

---
+++ 03Intermediate/14moreloops.js
@@ -0,0 +1,16 @@
+// const names = ["Instagram", "Facebook", "Hotstar", "Youtube"]
+
+// for(const n of names){
+//     console.log(n);
+// }
+
+const symbols = {
+    yt : "Youtube",
+    ig : "Instagram",
+    fb : "Facebook",
+    cr : "Crossroads"
+};
+
+for(const n in symbols){
+    console.log(symbols[n]);
+}
