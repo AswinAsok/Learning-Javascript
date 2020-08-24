f04ea37e256f1c8e4b3a927bcc7402f0dca90d9c
AswinAsok <aswinasokofficial@gmail.com>
2020-08-24 07:41:23 +0530

    Lexical scoping

---
+++ 06 Not Advance/Lexicalscoping.js
@@ -0,0 +1,11 @@
+function init(){
+    var firstname = "Aswin"
+    function sayFirstname(){
+        console.log(firstname);
+    }
+    sayFirstname();
+}
+
+init();
+console.log(firstname);
+
--- 06 Not Advance/anonymousfunction.js
+++ 06 Not Advance/anonymousfunction.js
@@ -2,4 +2,7 @@
     console.log("I say Hello");
     console.log("I again say Hello");
 
-})();
+})
+
+k=10
+console.log(10);
