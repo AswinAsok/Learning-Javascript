ba06a82f425eb6ca73568500cb39ec07a3fca764
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-16 15:30:09 +0530

    Classes and Objects

---
+++ 06Advanceish/06 classcall.js
@@ -0,0 +1,8 @@
+const User = require("./06classes")
+
+
+const Aswin = new User("Aswin", "aswinasokofficial@lco.com")
+
+Aswin.enrollCourse("Mern Bootcamp")
+Aswin.enrollCourse("React Bootcamp")
+console.log(Aswin.getCourseList());
