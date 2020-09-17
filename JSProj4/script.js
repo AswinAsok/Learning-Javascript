403c285afe752e18749c51e9ef8dc361ddf9c5d5
Aswin Asok <aswinasokofficial@gmail.com>
2020-09-17 20:46:38 +0530

    Almost Over

---
+++ JSProj4/script.js
@@ -0,0 +1,66 @@
+
+//Variable
+var isFlipped = false;
+var firstCard = null 
+var secondCard = null
+
+
+let cards = document.querySelectorAll('.card');
+console.log(cards);
+
+cards.forEach((card) => card.addEventListener("click" ,flip))
+
+function flip(){
+
+    this.classList.add("flip")
+    if(!isFlipped){
+        isFlipped = true;
+        firstCard = this
+    }
+    else{
+        secondCard = this
+        console.log(firstCard);
+        console.log(secondCard);
+        checkIt()
+    }
+}
+
+function checkIt(){
+    if (firstCard.dataset.image===secondCard.dataset.image)
+        success()
+    else
+        fail()
+}
+
+function success(){
+    firstCard.removeEventListener('click', flip)
+    secondCard.removeEventListener('click', flip)
+    reset();
+}
+
+function fail(){
+    setTimeout(()=>{
+        firstCard.classList.remove("flip")
+        secondCard.classList.remove("flip")
+        reset();
+    },750)}
+
+function reset(){
+    isFlipped = false;
+    firstCard = null;
+    secondCard = null
+    filpping = false;
+}
+
+
+
+
+//TODO Shuffle
+
+(function shuffle(){
+    cards.forEach((card)=>{
+        var index = Math.floor(Math.random()*16)
+        card.style.order = index; 
+    })
+})();
+
